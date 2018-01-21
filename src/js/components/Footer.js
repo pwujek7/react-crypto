548b001a658292b25491008d9f5c70ed271b41ed
pwujek7 <pwujek7@gmail.com>
2018-01-21 12:09:01 +0100

    added Footer component

---
+++ src/js/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Data provided by <a href="https://coinmarketcap.com/" target="_blank">CoinMarketCap</a> API </p>
+    </footer>
+  )
+};
+
+export default Footer;
