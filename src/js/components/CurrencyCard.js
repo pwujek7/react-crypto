30931a44e8e5aa6282d301be3ebb6620807f92f3
pwujek7 <pwujek7@gmail.com>
2018-01-25 12:38:06 +0100

    updated Card component

--- src/js/components/CurrencyCard.js
+++ src/js/components/CurrencyCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 class CurrencyCard extends React.Component {
+
   render() {
     let {id, name, symbol, price_usd, percent_change_1h, percent_change_24h} = this.props.curr;
 
@@ -8,13 +9,23 @@ class CurrencyCard extends React.Component {
       <div className="card">
         <div className="card__row">
           <p className="card__txt">{name} | {symbol}</p>
-          <p className="card__txt card__txt--price">{price_usd} $</p>
+          <p className="card__txt card__txt--number">{price_usd} $</p>
         </div>
         <div className="card__row card__row--between">
-          <p className="card__txt card__txt--percent">
-            <span className="card__txt card__txt--small">Last 1h: </span>{percent_change_1h}%</p>
-          <p className="card__txt card__txt--percent">
-            <span className="card__txt card__txt--small">Last 24h: </span>{percent_change_24h}%</p>
+          <p className="card__txt card__txt--percent"> Last 1h:
+            { percent_change_1h > 0 ? (
+              <span className="card__txt card__txt--number card__txt--green"> {percent_change_1h}%</span>
+            ) : (
+              <span className="card__txt card__txt--number card__txt--red"> {percent_change_1h}%</span>
+            )}
+          </p>
+          <p className="card__txt card__txt--percent"> Last 24h:
+            { percent_change_24h > 0 ? (
+              <span className="card__txt card__txt--number card__txt--green"> {percent_change_24h}%</span>
+            ) : (
+              <span className="card__txt card__txt--number card__txt--red"> {percent_change_24h}%</span>
+            )}
+          </p>
         </div>
       </div>
     )
